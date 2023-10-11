dfd419e5fbf42b63354368154135388c866d7b2c
Akkhazriel <akkhazriel@gmail.com>
2023-10-11 11:33:45 +0300

    add section home + mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import { toggleNavMenu } from "./modules/nav-menu";
+import { scroller } from "./modules/sponsors";
 // Call mobile menu when 999px
 toggleNavMenu();
+// Sponsors scroller
+scroller();
